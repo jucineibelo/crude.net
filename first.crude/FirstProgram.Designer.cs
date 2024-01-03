7f7d174f7d782904995c0b3e4b6516bc797c9c20
Juci <jucineibelo@gmail.com>
2024-01-03 00:17:58 -0300

    commit project

---
+++ first.crude/FirstProgram.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace first.crude
+{
+    partial class FirstProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblEdt = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.edtId = new System.Windows.Forms.TextBox();
+            this.edtName = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblTitle.Location = new System.Drawing.Point(178, 42);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(210, 24);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "SQLite Database Test";
+            // 
+            // lblEdt
+            // 
+            this.lblEdt.AutoSize = true;
+            this.lblEdt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEdt.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblEdt.Location = new System.Drawing.Point(34, 112);
+            this.lblEdt.Name = "lblEdt";
+            this.lblEdt.Size = new System.Drawing.Size(28, 17);
+            this.lblEdt.TabIndex = 1;
+            this.lblEdt.Text = "ID:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblName.Location = new System.Drawing.Point(34, 150);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(54, 17);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name:";
+            // 
+            // edtId
+            // 
+            this.edtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edtId.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.edtId.Location = new System.Drawing.Point(101, 109);
+            this.edtId.Name = "edtId";
+            this.edtId.Size = new System.Drawing.Size(192, 23);
+            this.edtId.TabIndex = 3;
+            // 
+            // edtName
+            // 
+            this.edtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edtName.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.edtName.Location = new System.Drawing.Point(101, 147);
+            this.edtName.Name = "edtName";
+            this.edtName.Size = new System.Drawing.Size(192, 23);
+            this.edtName.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.name});
+            this.dataGridView1.Location = new System.Drawing.Point(309, 109);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(265, 189);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // id
+            // 
+            this.id.FillWeight = 70F;
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            this.id.Width = 70;
+            // 
+            // name
+            // 
+            this.name.FillWeight = 150F;
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            this.name.Width = 150;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnInsert.Location = new System.Drawing.Point(37, 193);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(256, 31);
+            this.btnInsert.TabIndex = 6;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnUpdate.Location = new System.Drawing.Point(37, 230);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(256, 31);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnDelete.Location = new System.Drawing.Point(37, 267);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(256, 31);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 330);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.edtName);
+            this.Controls.Add(this.edtId);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblEdt);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblEdt;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox edtId;
+        private System.Windows.Forms.TextBox edtName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+    }
+}
+
---
+++ first.crude/FirstProgram.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace first.crude
+{
+    public partial class FirstProgram : Form
+    {
+        //path of database
+        string path = "data_table.db";
+        string cs = @"URI=file:"+Application.StartupPath+ "\\data_table.db"; //data create debug folder
+
+        SQLiteConnection conn;
+        SQLiteCommand cmd;
+        SQLiteDataReader dr;
+
+        public FirstProgram()
+        {
+            InitializeComponent();
+        }
+
+        //show data in table
+        private void data_show()
+        {
+            var conn = new SQLiteConnection(cs);
+            conn.Open();
+            string stm = "SELECT id,name FROM test";
+            var cmd = new SQLiteCommand(stm,conn);
+            dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                dataGridView1.Rows.Insert(0,dr.GetString(0), dr.GetString(1));
+            }
+        }
+
+        //Create database and table 
+        private void Create_db()
+        {
+            if (!System.IO.File.Exists(path))
+            {
+                SQLiteConnection.CreateFile(path);
+                using (var sqlite = new  SQLiteConnection(@"Data Source=" + path)) 
+                {
+                    sqlite.Open();
+                    string sql = "create table test (id varchar(12), name varchar(20))";
+                    SQLiteCommand command = new SQLiteCommand(sql,sqlite);
+                    command.ExecuteNonQuery();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Database cannot create");
+                return;
+            }
+        }
+
+        private void ClearEdts()
+        {
+            edtId.Clear();
+            edtName.Clear();
+        }
+
+        //insert data
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            var conn = new SQLiteConnection(cs);
+            conn.Open();
+            var cmd = new SQLiteCommand();
+
+            try
+            {
+                cmd.CommandText = "INSERT INTO test (id,name) VALUES (@id,@name)";
+                cmd.Parameters.AddWithValue("@id", edtId.Text);
+                cmd.Parameters.AddWithValue("@name", edtName.Text);
+                cmd.Connection = conn;
+                cmd.ExecuteNonQuery();
+                dataGridView1.Rows.Clear();
+                data_show();
+                ClearEdts();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Não foi possível inserir os dados");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        //update 
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            var conn = new SQLiteConnection(cs);
+            conn.Open();
+            var cmd = new SQLiteCommand(conn);
+
+            try
+            {
+                cmd.CommandText = "UPDATE test SET name=@Name WHERE id=@Id";
+                cmd.Prepare();
+                cmd.Parameters.AddWithValue("Id", edtId.Text);
+                cmd.Parameters.AddWithValue("@Name", edtName.Text);
+                cmd.ExecuteNonQuery();
+                dataGridView1.Rows.Clear();
+                data_show();
+                ClearEdts();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Não foi possível atualizar os dados. Erro: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var conn = new SQLiteConnection(cs);
+            conn.Open();
+            var cmd = new SQLiteCommand(conn);
+
+            try
+            {
+                cmd.CommandText = "DELETE FROM test WHERE id =@id";
+                cmd.Prepare();
+                cmd.Parameters.AddWithValue("@id", edtId.Text);
+                cmd.ExecuteNonQuery();
+                dataGridView1.Rows.Clear();
+                data_show();
+                ClearEdts();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Não foi possível excluir os dados. Erro: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+            {
+                dataGridView1.CurrentRow.Selected = true;
+                edtId.Text = dataGridView1.Rows[e.RowIndex].Cells["Id"].FormattedValue.ToString();
+                edtName.Text = dataGridView1.Rows[e.RowIndex].Cells["Name"].FormattedValue.ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Create_db();
+            data_show();
+        }
+    }
+}
